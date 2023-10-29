552e515274c20668d8cd6bf033fa97ba4ea54418
Md Asaduzaman <asadasad9282@gmail.com>
2023-10-30 00:03:48 +0600

    get all from db and get by id

--- src/app/modules/payment/payment.routes.ts
+++ src/app/modules/payment/payment.routes.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import { PaymentController } from './payment.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
-// router.get('/', PaymentController.initPayment);
-
-// router.get('/:id', auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN), PaymentController.getByIdFromDB);
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  PaymentController.getAllFromDB
+);
+router.get(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  PaymentController.getByIdFromDB
+);
 
 router.post('/init', PaymentController.initPayment);
 
